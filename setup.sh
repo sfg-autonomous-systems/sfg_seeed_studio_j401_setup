54ea26a528147567934f5b51ffdebeb89aeb9b3f
coblit <constantin.blessing@hs-esslingen.de>
2025-03-27 12:48:37 +0100

    Integrated set_power_mode into setup.sh..

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ for SCRIPT in "${SCRIPT_DIR}/scripts"/*.sh; do
     source "${SCRIPT}"
 done
 
+echo -e "${BOLD_GREEN}>>> Starting with setup.${RESET}"
+
 check_environment "${@}" || exit 1
 update_packages
 install_dependencies "${1}"
 fix_intel_ax210
 disable_wifi_power_management
+set_power_mode
+
+echo -e "${BOLD_GREEN}<<< Done with setup.${RESET}"
